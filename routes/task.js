fc10edb9f3f05da79dea8eca05680e4a33828a63
Sayan Nath <sayannath235@gmail.com>
2021-05-04 20:14:50 +0530

    Taks Routes Added

--- routes/task.js
+++ routes/task.js
@@ -3,10 +3,10 @@ var router = express();
 
 const Task = require("../controllers/task");
 
-router.post("/task/create");
-router.get("/task/:task_id");
-router.get("/tasks");
-router.put("/task/update/:task_id");
-router.delete("/task/delete/:task_id");
+router.post("/task/create", Task.createTask);
+router.get("/task/:task_id", Task.getTask);
+router.get("/tasks", Task.getAllTask);
+router.put("/task/update/:task_id", Task.updateTaskById);
+router.delete("/task/delete/:task_id", Task.deleteTaskById);
 
 module.exports = router;
