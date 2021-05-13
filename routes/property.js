e6bab51e69f4257067aa7368a0be801860c8f8d4
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-05-13 15:07:04 +0530

    Update property.js

--- routes/property.js
+++ routes/property.js
@@ -6,14 +6,14 @@ const Property = require("../controllers/property");
 //Create Property
 router.post("/property/create", Property.createProperty);
 //Get Property by PropertyId
-router.get("property/:property_id", Property.getPropertyByPropertyId);
+router.get("/property/:property_id", Property.getPropertyByPropertyId);
 //Get Property by OwnerId
-router.get("property/owner/:owner_id", Property.getPropertyByOwnerId);
+router.get("/property/owner/:owner_id", Property.getPropertyByOwnerId);
 //Get All Properties
-router.get("properties/", Property.getProperties);
+router.get("/properties/", Property.getProperties);
 //Update Properties
-router.put("property/update/:property_id", Property.updateProperty);
+router.put("/property/update/:property_id", Property.updateProperty);
 //Delete Properties
-router.delete("property/delete/:property_id", Property.deleteProperty);
+router.delete("/property/delete/:property_id", Property.deleteProperty);
 
 module.exports = router;
