a0a12cfff1c94b05263451e859460cb0d4cbc6c4
Sayan Nath <sayannath235@gmail.com>
2021-06-22 20:14:28 +0530

    Added another one

--- models/attendance.js
+++ models/attendance.js
@@ -66,7 +66,7 @@ Attendance.findAllAttendance = (result) => {
   ];
   dbConn.query(
     {
-      sql: "SELECT * FROM app_attendance JOIN tbl_users ON app_attendance.user_id = tbl_users.user_id ",
+      sql: "SELECT * FROM app_attendance JOIN tbl_users ON app_attendance.user_id = tbl_users.user_id ORDER BY punch_in DESC ",
       nestTables: true,
     },
     (err, res) => {
