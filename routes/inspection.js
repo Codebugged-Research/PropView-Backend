a4216223c49adc2f8fc111d7a29c3066b00653ce
Sayan Nath <sayannath235@gmail.com>
2021-07-11 00:05:05 +0530

    Yes I came back to fix the typos and I am sure that I will come back again

--- routes/inspection.js
+++ routes/inspection.js
@@ -3,18 +3,18 @@ const router = express();
 
 const Inspection = require("../controllers/inspection");
 
-router.post("inspection/create/", Inspection.createInspection);
-router.get("inspection/get/", Inspection.getInspection);
-router.get("inspection/get/:inspection_id", Inspection.getInspectionById);
+router.post("/inspection/create/", Inspection.createInspection);
+router.get("/inspection/get/", Inspection.getInspection);
+router.get("/inspection/get/:inspection_id", Inspection.getInspectionById);
 router.get(
-  "inspection/get/property/:property_id",
+  "/inspection/get/property/:property_id",
   Inspection.getInspectionByPropertyId
 );
 router.get(
-  "inspection/get/employee/:employee_id",
+  "/inspection/get/employee/:employee_id",
   Inspection.getInspectionByEmployeeId
 );
-router.put("inspection/update/:inspection_id", Inspection.updateInspection);
-router.delete("inspection/delete/:inspection_id", Inspection.deleteInspection);
+router.put("/inspection/update/:inspection_id", Inspection.updateInspection);
+router.delete("/inspection/delete/:inspection_id", Inspection.deleteInspection);
 
 module.exports = router;
