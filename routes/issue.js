ab0b791136574e99343f3e6d4ecc01e9b09a1af1
Sayan Nath <sayannath235@gmail.com>
2021-07-11 00:07:39 +0530

    Yes again again I came back! for a Typo

--- routes/issue.js
+++ routes/issue.js
@@ -3,10 +3,10 @@ const router = express();
 
 const Issue = require("../controllers/issue");
 
-router.post("issue/create/", Issue.createIssue);
-router.get("issue/get/", Issue.getIssues);
-router.get("issue/get/:issue_id", Issue.getIssuesById);
-router.put("issue/update/:issue_id", Issue.updateIssue);
-router.delete("issue/delete/:issue_id", Issue.deleteIssue);
+router.post("/issue/create/", Issue.createIssue);
+router.get("/issue/get/", Issue.getIssues);
+router.get("/issue/get/:issue_id", Issue.getIssuesById);
+router.put("/issue/update/:issue_id", Issue.updateIssue);
+router.delete("/issue/delete/:issue_id", Issue.deleteIssue);
 
 module.exports = router;
--- routes/issue_table.js
+++ routes/issue_table.js
@@ -3,20 +3,20 @@ const router = express.Router();
 
 const IssueTable = require("../controllers/issue_table");
 
-router.post("issue/table/create", IssueTable.createIssueTable);
-router.get("issue/table/get/:issue_table_id", IssueTable.getIssueTableById);
-router.get("issue/table/get/room/:room_id", IssueTable.getIssueTableByRoomId);
+router.post("/issue/table/create", IssueTable.createIssueTable);
+router.get("/issue/table/get/:issue_table_id", IssueTable.getIssueTableById);
+router.get("/issue/table/get/room/:room_id", IssueTable.getIssueTableByRoomId);
 router.get(
-  "issue/table/get/subroom/:subroom_id",
+  "/issue/table/get/subroom/:subroom_id",
   IssueTable.getIssueTableBySubRoomId
 );
 router.get(
-  "issue/table/get/property/:property_id",
+  "/issue/table/get/property/:property_id",
   IssueTable.getIssueTableByPropertyId
 );
-router.put("issue/table/update/:issue_table_id", IssueTable.updateIssueTable);
+router.put("/issue/table/update/:issue_table_id", IssueTable.updateIssueTable);
 router.delete(
-  "issue/table/delete/:issue_table_id",
+  "/issue/table/delete/:issue_table_id",
   IssueTable.deleteIssueTable
 );
 
