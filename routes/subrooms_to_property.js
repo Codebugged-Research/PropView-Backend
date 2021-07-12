18431b514bbb861a709ee5d7b6d1c4bcd2e5d9cf
Sayan Nath <sayannath235@gmail.com>
2021-07-12 14:28:42 +0530

    Sub Room API endpint fixed

--- routes/subrooms_to_property.js
+++ routes/subrooms_to_property.js
@@ -5,19 +5,19 @@ const SubRoomsProperty = require("../controllers/subrooms_to_property");
 
 router.post("/subroom/create", SubRoomsProperty.createSubRoomToProperty);
 
-router.get("subroom/get/", SubRoomsProperty.getAllSubRooms);
+router.get("/subroom/get/", SubRoomsProperty.getAllSubRooms);
 
 router.get(
-  "subroom/get/property/:property_id",
+  "/subroom/get/property/:property_id",
   SubRoomsProperty.getAllSubRoomByPropertyId
 );
 
 router.put(
-  "subroom/update/:property_sub_room_id",
+  "/subroom/update/:property_sub_room_id",
   SubRoomsProperty.updateSubRoom
 );
 router.delete(
-  "subroom/delete/:property_sub_room_id",
+  "/subroom/delete/:property_sub_room_id",
   SubRoomsProperty.deleteSubRoom
 );
 
