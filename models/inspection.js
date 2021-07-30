eeee11184421ddc94890872aa0ad07da1507aee8
Sayan Nath <sayannath235@gmail.com>
2021-07-30 11:26:18 +0530

    Inspection Model Changes done :)

--- models/inspection.js
+++ models/inspection.js
@@ -4,6 +4,16 @@ var func = require("node-mysql-nesting");
 var Inspection = function (inspection) {
   this.inspection_id = inspection.inspection_id;
   this.inspect_type = inspection.inspect_type;
+  this.maintainance_charges = inspection.maintainance_charges;
+  this.common_area_electricity = inspection.common_area_electricity;
+  this.electric_society = inspection.electric_society;
+  this.electric_authority = inspection.electric_authority;
+  this.power_backup = inspection.power_backup;
+  this.png_lpg = inspection.png_lpg;
+  this.club = inspection.club;
+  this.water = inspection.water;
+  this.property_tax = inspection.property_tax;
+  this.any_other = inspection.any_other;
   this.property_id = inspection.property_id;
   this.employee_id = inspection.employee_id;
   this.issue_id_list = inspection.issue_id_list; //List of id
@@ -74,11 +84,21 @@ Inspection.findInspectionByEmployeeId = (employee_id, result) => {
 
 Inspection.findByIdAndUpdate = (inspection_id, inspectionReqData, result) => {
   dbConn.query(
-    "UPDATE app_inspection SET inspection_id=?, inspect_type=?, property_id=?,employee_id=?,issue_id_list=? WHERE inspection_id=?",
+    "UPDATE app_inspection SET inspection_id=?, inspect_type=?,maintenance_charges=?, common_area_electricity=?, electricity_society=?, electricity_authority=?, power_backup=?, png_lgp=?, club=?, water=?, property_tax=?, any_other=?, property_id=?,employee_id=?,issue_id_list=? WHERE inspection_id=?",
     inspection_id,
     [
       inspectionReqData.inspection_id,
       inspectionReqData.inspect_type,
+      inspectionReqData.maintainance_charges,
+      inspectionReqData.common_area_electricity,
+      inspectionReqData.electric_society,
+      inspectionReqData.electric_authority,
+      inspectionReqData.power_backup,
+      inspectionReqData.png_lpg,
+      inspectionReqData.club,
+      inspectionReqData.water,
+      inspectionReqData.property_tax,
+      inspectionReqData.any_other,
       inspectionReqData.property_id,
       inspectionReqData.employee_id,
       inspectionReqData.issue_id_list,
