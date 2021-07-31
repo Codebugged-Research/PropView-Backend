a0120134facc01995ba4f8b141504697fc774fd8
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-07-31 13:56:49 +0530

    inspection upload

--- controllers/upload-image.js
+++ controllers/upload-image.js
@@ -1,5 +1,4 @@
 const aws = require("aws-sdk");
-const uuid = require("uuid");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 
@@ -62,3 +61,30 @@ exports.uploadPropertyImage = (req, res) => {
     res.json({ message: "success" });
   });
 };
+
+exports.uploadInspection = (req, res) => {
+  const spacesEndpoint = new aws.Endpoint(BUCKET_ENDPOINT);
+  const s3 = new aws.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: BUCKET_ACCESS_KEY,
+    secretAccessKey: BUCKET_SECRET_KEY,
+  });
+  const upload = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: "propview/Inspection",
+      acl: "public-read",
+      key: function (request, file, cb) {
+        cb(null, file.originalname);
+      },
+    }),
+  }).array("upload", 1);
+  upload(req, res, function (error) {
+    if (error) {
+      return res.status(400).json({
+        error: error,
+      });
+    }
+    res.json({ message: "success" });
+  });
+}
--- routes/upload-image.js
+++ routes/upload-image.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {uploadImage, uploadPropertyImage} = require("../controllers/upload-image");
+const {uploadImage, uploadPropertyImage, uploadInspection} = require("../controllers/upload-image");
 
-router.post("/upload/image", uploadImage)
+router.post("/upload/image", uploadImage);
 router.post("/upload/property", uploadPropertyImage);
+router.post("/upload/inspection", uploadInspection);
 
 module.exports = router;
