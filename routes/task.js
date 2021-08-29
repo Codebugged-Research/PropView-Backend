91ebae9b7f8892b47a912c7e70cd50f26302daf3
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-08-29 11:47:04 +0530

    Update task.js

--- routes/task.js
+++ routes/task.js
@@ -10,16 +10,14 @@ router.get("/task/:task_id", Task.getTask);
 router.get("/tasks", Task.getAllTask);
 
 router.get("/task/user/:assigned_to", Task.getAllTaskByUser);
-
 router.get("/task/pending/:assigned_to", Task.getPendingTaskByUserId);
-
 router.get("/task/team/:assigned_to", Task.getAllTaskWithoutUser);
-
 router.post("/task/manager/", Task.getAllTaskByManger);
 
+//new
 router.post("/new/task/self", Task.getSelfTask);
-
 router.post("/new/task/team", Task.getTeamTask);
+
 
 router.put("/task/update/:task_id", Task.updateTaskById);
 
