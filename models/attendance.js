03478aed5927ba8a37c3066d2fe9cfc458af1ccb
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-09-24 12:20:46 +0530

    Update attendance.js

--- models/attendance.js
+++ models/attendance.js
@@ -178,7 +178,7 @@ Attendance.findAttendanceByParentId = (parent_id, result) => {
   ];
   dbConn.query(
     {
-      sql: "SELECT * FROM app_attendance JOIN tbl_users ON app_attendance.user_id = tbl_users.user_id WHERE app_attendance.parent_id=? ORDER BY attendance_id",
+      sql: "SELECT * FROM app_attendance JOIN tbl_users ON app_attendance.user_id = tbl_users.user_id WHERE app_attendance.parent_id=? ORDER BY attendance_id DESC",
       nestTables: true,
     },
     parent_id,
