4fa121c7355141b142f16fa8f02eb015095494c7
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-02-23 21:15:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(cookieParser());
 app.use(cors());
 
 app.get("/version", (req, res) => {
-  res.json("1.0.38+39");
+  res.json("1.0.40+41");
 });
 
 app.use('/404', express.static('public/404'));
