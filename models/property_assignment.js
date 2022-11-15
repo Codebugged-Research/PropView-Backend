bfb914f2ec3ec58d3264d2c7c4825ffa23841791
Weframe Tech <majhisambit2@gmail.com>
2022-11-15 13:24:31 +0530

    Update property_assignment.js

--- models/property_assignment.js
+++ models/property_assignment.js
@@ -9,7 +9,7 @@ var PropertyAssignment = function (property_assignment) {
 //Create Property Assignment
 PropertyAssignment.savePropertyAssignment = (propertyReqData, result) => {
   dbConn.query(
-    "INSERT INTO tbl_suser_to_property SET ?",
+    "INSERT INTO tbl_property_to_user SET ?",
     propertyReqData,
     (err, res) => {
       if (err) {
@@ -23,7 +23,7 @@ PropertyAssignment.savePropertyAssignment = (propertyReqData, result) => {
 //Get Property Assignment by User ID
 PropertyAssignment.findByUserId = (user_id, result) => {
   dbConn.query(
-    "SELECT * FROM tableproperty JOIN property_owner ON tableproperty.owner_id = property_owner.owner_id JOIN tbl_country ON tableproperty.cid = tbl_country.cid JOIN tbl_state ON tableproperty.sid = tbl_state.sid JOIN tbl_city ON tableproperty.ccid = tbl_city.ccid  JOIN tbl_locality ON tableproperty.locid = tbl_locality.locid JOIN tbl_society ON tableproperty.socid = tbl_society.socid WHERE tableproperty.status=1 AND FIND_IN_SET(property_id, (SELECT property_id from tbl_suser_to_property where user_id=?))",
+    "SELECT * FROM tableproperty JOIN property_owner ON tableproperty.owner_id = property_owner.owner_id JOIN tbl_country ON tableproperty.cid = tbl_country.cid JOIN tbl_state ON tableproperty.sid = tbl_state.sid JOIN tbl_city ON tableproperty.ccid = tbl_city.ccid  JOIN tbl_locality ON tableproperty.locid = tbl_locality.locid JOIN tbl_society ON tableproperty.socid = tbl_society.socid WHERE tableproperty.status=1 AND FIND_IN_SET(property_id, (SELECT property_id from tbl_property_to_user where user_id=?))",
     [user_id],
     (err, res) => {
       if (err) {
@@ -36,7 +36,7 @@ PropertyAssignment.findByUserId = (user_id, result) => {
 
 PropertyAssignment.isPropertyAssigned = (id_1, id_2, id_3, id_4, result) => {
   dbConn.query(
-    "SELECT * FROM tbl_suser_to_property WHERE property_id LIKE ? OR property_id LIKE ? OR property_id LIKE ? OR property_id=?",
+    "SELECT * FROM tbl_property_to_user WHERE property_id LIKE ? OR property_id LIKE ? OR property_id LIKE ? OR property_id=?",
     [id_1, id_2, id_3, id_4],
     (err, res) => {
       if (err) {
@@ -49,7 +49,7 @@ PropertyAssignment.isPropertyAssigned = (id_1, id_2, id_3, id_4, result) => {
 
 PropertyAssignment.getRowByUserId = (id, result) => {
   dbConn.query(
-    "SELECT * FROM tbl_suser_to_property WHERE user_id=?",
+    "SELECT * FROM tbl_property_to_user WHERE user_id=?",
     [id],
     (err, res) => {
       if (err) {
@@ -62,7 +62,7 @@ PropertyAssignment.getRowByUserId = (id, result) => {
 
 PropertyAssignment.isPropertyAssigned0 = (id_1, id_2, id_3, id_4, result) => {
   dbConn.query(
-    "SELECT * FROM tbl_user_to_property WHERE property_id LIKE ? OR property_id LIKE ? OR property_id LIKE ? OR property_id=?",
+    "SELECT * FROM tbl_property_to_user WHERE property_id LIKE ? OR property_id LIKE ? OR property_id LIKE ? OR property_id=?",
     [id_1, id_2, id_3, id_4],
     (err, res) => {
       if (err) {
@@ -80,7 +80,7 @@ PropertyAssignment.findByIdAndUpdate = (
   result
 ) => {
   dbConn.query(
-    "UPDATE tbl_suser_to_property SET user_to_property_id=?, user_id=?, property_id=? WHERE user_to_property_id=?",
+    "UPDATE tbl_property_to_user SET user_to_property_id=?, user_id=?, property_id=? WHERE user_to_property_id=?",
     [
       reqUserPropertyData.user_to_property_id,
       reqUserPropertyData.user_id,
