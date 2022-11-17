66a1f4070fa598bf3333ec72ec9144bf852e5dc4
Weframe Tech <majhisambit2@gmail.com>
2022-11-17 23:25:24 +0530

    Update property_assignment.js

--- models/property_assignment.js
+++ models/property_assignment.js
@@ -1,7 +1,7 @@
 var dbConn = require("../config/database");
 
 var PropertyAssignment = function (property_assignment) {
-  this.user_to_property_id = property_assignment.user_to_property_id;
+  this.id = property_assignment._id;
   this.user_id = property_assignment.user_id;
   this.property_id = property_assignment.property_id;
 };
@@ -23,7 +23,7 @@ PropertyAssignment.savePropertyAssignment = (propertyReqData, result) => {
 //Get Property Assignment by User ID
 PropertyAssignment.findByUserId = (user_id, result) => {
   dbConn.query(
-    "SELECT * FROM tableproperty JOIN property_owner ON tableproperty.owner_id = property_owner.owner_id JOIN tbl_country ON tableproperty.cid = tbl_country.cid JOIN tbl_state ON tableproperty.sid = tbl_state.sid JOIN tbl_city ON tableproperty.ccid = tbl_city.ccid  JOIN tbl_locality ON tableproperty.locid = tbl_locality.locid JOIN tbl_society ON tableproperty.socid = tbl_society.socid WHERE tableproperty.status=1 AND FIND_IN_SET(property_id, (SELECT property_id from tbl_property_to_user where user_id=?))",
+    "SELECT * FROM tableproperty JOIN property_owner ON tableproperty.owner_id = property_owner.owner_id JOIN tbl_country ON tableproperty.cid = tbl_country.cid JOIN tbl_state ON tableproperty.sid = tbl_state.sid JOIN tbl_city ON tableproperty.ccid = tbl_city.ccid  JOIN tbl_locality ON tableproperty.locid = tbl_locality.locid JOIN tbl_society ON tableproperty.socid = tbl_society.socid WHERE tableproperty.status=1 AND property_id in (SELECT property_id from tbl_property_to_user where user_id=?)",
     [user_id],
     (err, res) => {
       if (err) {
@@ -80,9 +80,9 @@ PropertyAssignment.findByIdAndUpdate = (
   result
 ) => {
   dbConn.query(
-    "UPDATE tbl_property_to_user SET user_to_property_id=?, user_id=?, property_id=? WHERE user_to_property_id=?",
+    "UPDATE tbl_property_to_user SET id=?, user_id=?, property_id=? WHERE user_to_property_id=?",
     [
-      reqUserPropertyData.user_to_property_id,
+      reqUserPropertyData.id,
       reqUserPropertyData.user_id,
       reqUserPropertyData.property_id,
       userToPropertyId,
